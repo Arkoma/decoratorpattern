a0d2e646bb48c8c751ed7f16dc1cf7152926dd70
Aaron Burk <aburk@veracode.com>
2024-01-24 09:45:06 -0500

    Add ingredients that can help subclasses customize without creating a new class for every combination of ingredients

--- src/main/java/com/example/decoratorpattern/Beverage.java
+++ src/main/java/com/example/decoratorpattern/Beverage.java
@@ -4,6 +4,44 @@ public abstract class Beverage {
 
     String description = "";
 
+    boolean milk;
+    boolean soy;
+
+    public boolean hasMilk() {
+        return milk;
+    }
+
+    public void setMilk(boolean milk) {
+        this.milk = milk;
+    }
+
+    public boolean hasSoy() {
+        return soy;
+    }
+
+    public void setSoy(boolean soy) {
+        this.soy = soy;
+    }
+
+    public boolean hasMocha() {
+        return mocha;
+    }
+
+    public void setMocha(boolean mocha) {
+        this.mocha = mocha;
+    }
+
+    public boolean hasWhip() {
+        return whip;
+    }
+
+    public void setWhip(boolean whip) {
+        this.whip = whip;
+    }
+
+    boolean mocha;
+    boolean whip;
+
     public String getDescription() {
         return this.description;
     }
